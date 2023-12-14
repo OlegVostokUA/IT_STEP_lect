10a8f6343b14dd8d86c27a53ae989114110637f5
OlegVostokUA <93160512+OlegVostokUA@users.noreply.github.com>
2023-12-14 16:56:34 +0200

    Update material.py

--- lect_05/material.py
+++ lect_05/material.py
@@ -511,3 +511,10 @@ for i in st:
 print(*lst1)
 print(*lst2)
 
+###
+keys = ['модель', 'цена', 'количество', 'размер', 'цвет', 'скидка']
+values = ['XXLDude', 5678.00, 57, 'XXL', 'черный', '12%']
+
+res = [key + ': ' + str(value) for key, value in zip(keys, values)]
+
+print(*res, sep='\n')
