a31e0f6bb0f48c34755c035946a07b1610628063
OlegVostok <vostokukrdon@gmail.com>
2024-01-09 17:15:14 +0200

    09.01 Notebook

---
+++ lect_10/lect_10.py
@@ -0,0 +1,8 @@
+numb = [2, 50, 35, 12]
+numb2 = ['W', 'G', 'L', 'X']
+
+
+for s, n in zip(numb, numb2):
+    if s == 'L' or n == 'L':
+        print(';find')
+
--- lect_10/material.py
+++ lect_10/material.py
@@ -53,37 +53,265 @@ def mult(n):
 #     print("Wrong input!")
 
 ### CASE 3
-adminPassw='111'
-studentPassw='222'
+# adminPassw='111'
+# studentPassw='222'
+#
+# def userLogIn(userLog, userPass):
+#     if (userLog.lower() == 'admin') and (userPass == adminPassw):
+#         print("Welcome, admin")
+#     elif (userLog.lower() == 'student') and (userPass == studentPassw):
+#         print("Welcome, student")
+#     else:
+#         print("Wrong data")
+#
+# def changePass(userLog, userPass):
+#     global adminPassw
+#     global studentPassw
+#     if (userLog.lower() == 'admin') and (userPass == adminPassw):
+#         adminPassw = input("Input new password for admin:")
+#     elif (userLog.lower() == 'student') and (userPass == studentPassw):
+#         studentPassw = input("Input new password for student:")
+#     else:
+#         print("Wrong data")
+#
+# def userAction(login, passw, action):
+#     return action(login, passw)
+#
+# userL=input("Login: ")
+# userP=input("Password: ")
+# userAnsw=input("1: LogIn; 2: Change password ")
+#
+# if userAnsw == '1':
+#     userAction(userL, userP, userLogIn)
+# elif userAnsw == '2':
+#     userAction(userL, userP, changePass)
+# else:
+#     print("Wrong data")
 
-def userLogIn(userLog, userPass):
-    if (userLog.lower() == 'admin') and (userPass == adminPassw):
-        print("Welcome, admin")
-    elif (userLog.lower() == 'student') and (userPass == studentPassw):
-        print("Welcome, student")
-    else:
-        print("Wrong data")
+###### carring
+### CASE 1
+# def sendMsg(userTo, msgTxt):
+#     print(f"Dear {userTo}, welcome to Python world! {msgTxt}")
+#
+# sendMsg('admin', 'Have a nice day!')
+# sendMsg('admin', 'See you!')
+# sendMsg('admin', 'Good luck!')
+# sendMsg('student', 'Good luck!')
 
-def changePass(userLog, userPass):
-    global adminPassw
-    global studentPassw
-    if (userLog.lower() == 'admin') and (userPass == adminPassw):
-        adminPassw = input("Input new password for admin:")
-    elif (userLog.lower() == 'student') and (userPass == studentPassw):
-        studentPassw = input("Input new password for student:")
-    else:
-        print("Wrong data")
+### CASE 2
+# def sendMsg(userTo):
+#     def setMsg(msgTxt):
+#         print(f"Dear {userTo}, welcome to Python world! {msgTxt}")
+#
+#     return setMsg
+#
+# user_admin = sendMsg('Admin')
+# user_admin('Have a nice day')
+# user_admin('See you')
+# user_admin('Good luck1')
+# sendMsg('Student')('Hello')
 
-def userAction(login, passw, action):
-    return action(login, passw)
+### CASE 3
+# def sendMsg(userTo):
+#     def setMsg(msgTxt):
+#         def setUserFrom(userFrom):
+#             def setLang(lang):
+#                 print(f'Dear {userTo}, Hello from {userFrom}.'
+#                       f'Welcome to {lang} world! {msgTxt}')
+#
+#             return setLang
+#         return setUserFrom
+#     return setMsg
+#
+# case1 = sendMsg('Admin')('Hello')('superuser')
+# case2 = sendMsg('Student')('Good luck!!!')
+#
+# case1('C++')
+# case2('Teacher')('Python')
 
-userL=input("Login: ")
-userP=input("Password: ")
-userAnsw=input("1: LogIn; 2: Change password ")
+##### decorators
+### CASE 1
+# def string_print():
+#     print('Hello. Happy new year!!!')
+#
+# var = 'Hello. Happy new year!!!'
+#
+# print('case 1')
+# string_print()
 
-if userAnsw == '1':
-    userAction(userL, userP, userLogIn)
-elif userAnsw == '2':
-    userAction(userL, userP, changePass)
-else:
-    print("Wrong data")
+# ### CASE 2
+# print('case 2')
+# print('*   *   *   *   *')
+# print('  *   *   *   *')
+# string_print()
+# print('  *   *   *   *')
+# print('*   *   *   *   *')
+#
+# ### CASE 3
+# print('case 3')
+# print('* * * * * * * * * *')
+# print(' * * * * * * * * *')
+# string_print()
+# print(' * * * * * * * * *')
+# print('* * * * * * * * * *')
+
+# ###CASE 4
+# def decorator_1(my_function):
+#     print('Im decorator #1')
+#     def wrapper():
+#         print('*   *   *   *   *')
+#         print('  *   *   *   *')
+#         my_function()
+#         print('  *   *   *   *')
+#         print('*   *   *   *   *')
+#     return wrapper
+#
+# def decorator_2(my_function):
+#     print('Im decorator #2')
+#     def wrapper():
+#         print('* * * * * * * * * *')
+#         print(' * * * * * * * * *')
+#         my_function()
+#         print(' * * * * * * * * *')
+#         print('* * * * * * * * * *')
+#     return wrapper
+#
+#
+# @decorator_2
+# @decorator_1
+# def string_print():
+#     print('Hello. Happy new year!!!')
+#
+#
+# string_print()
+#
+
+# def decorator_1(my_function):
+#     # Створюємо нову функцію
+#     def decorated_function(*args, **kwargs):
+#         print('decorator 1 start')
+#         my_function(*args, **kwargs) # Виклик функції
+#         print('decorator 1 finish')
+#
+#     # Повертаємо декоровану функцію
+#     return decorated_function
+#
+# def decorator2(my_function):
+#     # Створюємо нову функцію
+#     def decorated_function(*args, **kwargs):
+#         print('     decorator 2 start')
+#         my_function(*args, **kwargs) # Виклик функції
+#         print('     decorator 2 finish')
+#     # Повертаємо декоровану функцію
+#     return decorated_function
+#
+#
+#
+# @decorator1
+# @decorator2
+# def seyHello():
+#     print('             SAY HELLO FUNC')
+#
+# seyHello()
+
+# ### CASE 5
+# def decorator_1(my_function):
+#     print('Im decorator #1')
+#     def wrapper(var_in_f):
+#         print('*   *   *   *   *')
+#         print('  *   *   *   *')
+#         my_function(var_in_f)
+#         print('  *   *   *   *')
+#         print('*   *   *   *   *')
+#     return wrapper
+#
+# def decorator_2(my_function):
+#     print('Im decorator #2')
+#     def wrapper(var_in_f):
+#         print('* * * * * * * * * *')
+#         print(' * * * * * * * * *')
+#         my_function(var_in_f)
+#         print(' * * * * * * * * *')
+#         print('* * * * * * * * * *')
+#     return wrapper
+#
+#
+# @decorator_1
+# def string_print(var_in_f):
+#     print(var_in_f)
+#
+# var = 'Hello. Happy new year!!!'
+#
+# string_print(var)
+
+
+# ### CASE 6
+# def decorator_1(my_function):
+#     print('Im decorator #1')
+#     def wrapper(var_in_f):
+#         print('*   *   *   *   *')
+#         print('  *   *   *   *')
+#         a = my_function(var_in_f)
+#         print('#' * a)
+#         print('  *   *   *   *')
+#         print('*   *   *   *   *')
+#     return wrapper
+#
+#
+#
+# @decorator_1
+# def string_print(var_in_f):
+#     return var_in_f * 2
+#
+# var = 22
+#
+# # print(string_print(var))
+# string_print(var)
+
+
+
+# ### CASE 7
+# import time
+#
+# def decorator_1(my_function):
+#     def wrapper(*args):
+#         current_time = time.time()
+#         rez = my_function(*args)
+#         delta_time = time.time() - current_time
+#         print(f'Час виконання: {delta_time}')
+#         # print(rez)
+#         # return rez
+#     return wrapper
+#
+#
+# @decorator_1
+# def list_gen(a, b):
+#     lst = []
+#     for i in range(a, b):
+#         i += 1
+#         lst.append(i)
+#     print('DONE')
+#     return lst
+#
+# list_gen(0, 30000000)
+#print(s)
+
+
+# ### CASE 8
+def num_decorator(n):
+    def decorator_1(function):
+        def wrapper(*args):
+            stars = (n * "*")
+            result = function(*args)
+            result = stars, result, stars
+            return result
+
+        return wrapper
+    return decorator_1
+
+
+@num_decorator(25)
+def calculate(a, b):
+    return (a+b) * 2
+
+calculate(2, 25)
